a9841f7217a750336f97e2f503b092424b07cf0d
qizekun <765136104@qq.com>
2022-10-01 22:23:03 +0800

    new

---
+++ main.py
@@ -0,0 +1,87 @@
+import os
+import time
+import mitsuba as mi
+from utils import load, standardize_bbox, colormap, get_xml, fps
+import simple3d
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser('Point Cloud Visualizer')
+    parser.add_argument('--render', help='using mitsuba3 create beautiful image', action='store_true')
+    parser.add_argument('--tool', help='using real time point cloud visualization tools', action='store_true')
+    parser.add_argument('--knn', help='using knn',  action='store_true')
+    parser.add_argument('--white', help='white color',  action='store_true')
+    parser.add_argument('--path', type=str, help='the input file path', default='plane.ply')
+    parser.add_argument('--num', type=int, help='downsample point num', default=1024)
+    parser.add_argument('--center_num', type=int, help='knn center num', default=12)
+    parser.add_argument('--workdir', type=str, help='workdir', default='workdir')
+    parser.add_argument('--output', type=str, help='output file name', default='result.jpg')
+    parser.add_argument('--resolution', type=list, help='output file resolution', default=[1920, 1080])
+    parser.add_argument('--radius', type=float, help='radius', default=0.025)
+
+    args = parser.parse_args()
+    return args
+
+def render(config, pcl):
+
+    file_name = config.path.split('.')[0]
+    pcl = pcl[:, [2, 0, 1]]
+    pcl[:, 0] *= -1
+    pcl[:, 2] += 0.0125
+
+    if config.knn:
+        knn_center = fps(pcl, config.center_num)
+        knn_center += 0.5
+        knn_center[:, 2] -= 0.0125
+    else:
+        knn_center = []
+    if config.white:
+        white = True
+    else:
+        white = False
+
+    xml_head, xml_ball_segment, xml_tail = get_xml(config.resolution, config.radius)
+    xml_segments = [xml_head]
+    for i in range(pcl.shape[0]):
+        color = colormap(pcl[i, 0] + 0.5, pcl[i, 1] + 0.5, pcl[i, 2] + 0.5 - 0.0125, knn_center, white)
+        xml_segments.append(xml_ball_segment.format(pcl[i, 0], pcl[i, 1], pcl[i, 2], *color))
+    xml_segments.append(xml_tail)
+
+    xml_content = str.join('', xml_segments)
+
+    os.makedirs(config.workdir, exist_ok=True)
+    xmlFile = f'{config.workdir}/{file_name}.xml'
+    with open(xmlFile, 'w') as f:
+        f.write(xml_content)
+    f.close()
+
+    mi.set_variant("scalar_rgb")
+    scene = mi.load_file(xmlFile)
+    image = mi.render(scene, spp=256)
+    mi.util.write_bitmap(config.output, image)
+    # To prevent errors in the output image, we delay two seconds
+    time.sleep(2)
+    os.remove(xmlFile)
+
+def real_time_tool(config, pcl):
+    simple3d.showpoints(pcl, config)
+
+def main():
+    config = parse_args()
+    if config.render and config.tool:
+        raise RuntimeWarning('both render and real time tool are selected')
+    if config.render is False and config.tool is False:
+        raise RuntimeWarning('you need to choose one of render or real time tool')
+    
+
+    path = config.path
+    pcl = load(path)
+    pcl = standardize_bbox(pcl, config.num)
+
+    if config.render:
+        render(config, pcl)
+    else:
+        real_time_tool(config, pcl)
+
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,156 @@
+import numpy as np
+from plyfile import PlyData
+
+
+def load(path, Separator=';'):
+    extension = path.split('.')[-1]
+    if extension == 'npy':
+        pcl = np.load(path, allow_pickle=True)
+    elif extension == 'npz':
+        pcl = np.load(path)
+        pcl = pcl['pred']
+    elif extension == 'ply':
+        ply = PlyData.read(path)
+        vertex = ply['vertex']
+        (x, y, z) = (vertex[t] for t in ('x', 'y', 'z'))
+        pcl = np.column_stack((x, y, z))
+    elif extension == 'txt':
+        f = open(path,'r') 
+        line = f.readline() 
+        data = []
+        while line:
+            x,y,z = line.split(Separator)
+            data.append([float(x), float(y), float(z)])
+            line = f.readline()
+        f.close() 
+        pcl = np.array(data)
+    else:
+        print('unsupported file format.')
+        raise FileNotFoundError
+    return pcl
+
+def colormap(x, y, z, knn_center=[], white=False):
+
+    if white:
+        return [0.6, 0.6, 0.6]
+
+    vec = np.array([x, y, z])
+    if knn_center != []:
+        temp = abs(knn_center[:, 0] - x) + abs(knn_center[:, 1] - y) + abs(knn_center[:, 2] - z)
+        index = np.argmin(temp)
+        vec = knn_center[index]
+
+    vec = np.clip(vec, 0.0008, 1.0)
+    norm = np.sqrt(np.sum(vec ** 2))
+    vec /= norm
+
+    return [vec[0], vec[1], vec[2]]
+
+
+def standardize_bbox(pcl, points_per_object):
+    pt_indices = np.random.choice(pcl.shape[0], points_per_object, replace=False)
+    np.random.shuffle(pt_indices)
+    pcl = pcl[pt_indices]  # n by 3
+    mins = np.amin(pcl, axis=0)
+    maxs = np.amax(pcl, axis=0)
+    center = (mins + maxs) / 2.
+    scale = np.amax(maxs - mins)
+    print("Center: {}, Scale: {}".format(center, scale))
+    result = ((pcl - center) / scale).astype(np.float32)  # [-0.5, 0.5]
+    return result
+
+
+def fps(points, k):
+    sample_points = np.zeros((k, 3))
+    
+    # 用离重心最远的初始化
+    barycenter = np.sum(points, axis=0)/points.shape[0]
+    print(barycenter)
+    distance = np.full((points.shape[0]), np.nan)
+    point = barycenter
+    
+    # # 随机初始化
+    # point = points[0]
+    # points = points[:-1,:]
+    # sample_points[0,:] = point
+    # distance = np.sum((points - point)**2, axis=1)
+    for i in range(k):
+        distance = np.minimum(distance, np.sum((points - point)**2, axis=1)) # 前缀思想更新最小值
+        index = np.argmax(distance)
+        point = points[index]
+        sample_points[i,:] = point
+        mask = np.ones((points.shape[0]), dtype=bool)
+        mask[index] = False
+        points = points[mask]
+        distance = distance[mask]
+    return sample_points
+
+
+def get_xml(resolution=[1920, 1080], radius=0.025):
+    xml_head = \
+    f"""
+    <scene version="0.6.0">
+        <integrator type="path">
+            <integer name="maxDepth" value="-1"/>
+        </integrator>
+        <sensor type="perspective">
+            <float name="farClip" value="100"/>
+            <float name="nearClip" value="0.1"/>
+            <transform name="toWorld">
+                <lookat origin="3,3,3" target="0,0,0" up="0,0,1"/>
+            </transform>
+            <float name="fov" value="25"/>
+            <sampler type="independent">
+                <integer name="sampleCount" value="256"/>
+            </sampler>
+            <film type="hdrfilm">
+                <integer name="width" value="{resolution[0]}"/>
+                <integer name="height" value="{resolution[1]}"/>
+                <rfilter type="gaussian"/>
+            </film>
+        </sensor>
+
+        <bsdf type="roughplastic" id="surfaceMaterial">
+            <string name="distribution" value="ggx"/>
+            <float name="alpha" value="0.05"/>
+            <float name="intIOR" value="1.46"/>
+            <rgb name="diffuseReflectance" value="1,1,1"/> <!-- default 0.5 -->
+        </bsdf>
+
+    """
+
+    xml_ball_segment = \
+    f"""
+        <shape type="sphere">
+            <float name="radius" value="{radius}"/>
+            <transform name="toWorld">
+                <translate x="{"{}"}" y="{"{}"}" z="{"{}"}"/>
+            </transform>
+            <bsdf type="diffuse">
+                <rgb name="reflectance" value="{"{}"},{"{}"},{"{}"}"/>
+            </bsdf>
+        </shape>
+    """
+
+    xml_tail = \
+    """
+        <shape type="rectangle">
+            <ref name="bsdf" id="surfaceMaterial"/>
+            <transform name="toWorld">
+                <scale x="10" y="10" z="1"/>
+                <translate x="0" y="0" z="-0.5"/>
+            </transform>
+        </shape>
+
+        <shape type="rectangle">
+            <transform name="toWorld">
+                <scale x="10" y="10" z="1"/>
+                <lookat origin="-4,4,20" target="0,0,0" up="0,0,1"/>
+            </transform>
+            <emitter type="area">
+                <rgb name="radiance" value="6,6,6"/>
+            </emitter>
+        </shape>
+    </scene>
+    """
+    return xml_head, xml_ball_segment, xml_tail
